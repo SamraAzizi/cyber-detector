66fa779a8c618a1e5a49e323628af0f96a3cfcdc
Eltaf-azizi <iltfhuss@gmail.com>
2025-06-21 18:53:18 +0500

    start working on the model tuner

--- ml/training/tune_model.py
+++ ml/training/tune_model.py
@@ -10,3 +10,23 @@ from pathlib import Path
 import numpy as np
 
 logger = logging.getLogger(__name__)
+
+
+class ModelTuner:
+    def __init__(self, model_type='random_forest'):
+        self.config = load_config()
+        self.model_type = model_type
+        self.evaluator = ModelEvaluator()
+        self.study = None
+        
+    def load_data(self):
+        """Load processed training data"""
+        X_train = pd.read_csv('ml/datasets/processed/X_train.csv')
+        y_train = pd.read_csv('ml/datasets/processed/y_train.csv').squeeze()
+        return X_train, y_train
+        
+    def objective(self, trial):
+        """Objective function for Optuna optimization"""
+        X_train, y_train = self.load_data()
+
+        
